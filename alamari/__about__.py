66edea2b530b9771bc02f724ff8ab66df1b2d97c
Siddharth Belbase <23023761+sidbelbase@users.noreply.github.com>
2021-06-01 19:51:50 +0545

    👷 add about

---
+++ alamari/__about__.py
@@ -0,0 +1,7 @@
+__package_name__ = 'alamari'
+__version__ = '0.0.1'
+__author__ = 'Siddharth Belbase'
+__author_email__ = 'siddharthbelbase@gmail.com'
+__description__ = (
+    'A collection of utils that you might not require but might need.')
+__url__ = 'https://github.com/sidbelbase/alamari'
