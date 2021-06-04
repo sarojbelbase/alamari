76f1f579ed67b28409f70cd9cf9bd9c923a4de6f
Siddharth Belbase <23023761+sidbelbase@users.noreply.github.com>
2021-06-04 12:07:57 +0545

    ✨ add humanizer

---
+++ alamari/humanize.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from typing import Optional, Union
+
+import arrow
+
+Number = Union[int, float, str]
+
+
+def number(any_num: Number, padding: Optional[bool] = False) -> str:
+    """Formats number in nepali counting form replacing the international format
+
+    Args:
+        any_num (Number): The number to be formatted
+        padding (bool, optional): Defaults to False. If padding is required in any number less than 10.
+
+    Syntax:
+    >>> humanize_number(any_num, padding)
+
+    Example:
+
+    `any_num` = `9`, `padding` = `True`
+    >>> 09
+
+    `any_num` = `9.54`, `padding` = `True`
+    >>> 09
+
+    Returns:
+        str: returns a string in comma separated number
+    """
+    int_part, *float_part = str(any_num).partition(".")
+    separated_num = [int_part[x-2:x]
+                     for x in range(-3, -len(int_part), -2)][::-1] + [int_part[-3:]]
+    number_with_commas = ",".join(separated_num)
+    if padding and int(int_part) < 10:
+        return f"0{int_part}"
+    return "".join([number_with_commas] + float_part)
+
+
+def date(any_date: datetime) -> str:
+    """Converts any given date into humanized form (UTC)
+
+    Syntax:
+    >>> date(any_date)
+
+    Example:
+
+    `any_date` = `2021-06-02 14:55:34.185035`
+    >>> 2 hours ago
+
+    Args:
+        any_date (datetime): any date with datetime format
+
+    Returns:
+        [type]: Returns date in humanized form
+    """
+    return arrow.get(any_date).to('US/Pacific').humanize()
+
+
+def nepal_date(local_date: datetime) -> str:
+    """Converts datetime into humanized form (NPT)
+
+    Syntax:
+    >>> date(local_date)
+
+    Example:
+
+    `local_date` = `2021-06-02 14:55:34.185035`
+    >>> 2 hours ago
+
+    Args:
+        local_date (datetime): any local date with datetime format
+
+    Returns:
+        [type]: Returns date in humanized form
+    """
+    return arrow.get(local_date).shift(minutes=-345).to('US/Pacific').humanize()
